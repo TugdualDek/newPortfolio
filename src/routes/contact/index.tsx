17e88a22858195e4d6427ec3acd85d5c86ec64b9
Tugdual de K <tuak61273@eleve.isep.fr>
2023-03-06 22:18:48 +0100

    animation header

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -2,24 +2,10 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  /* const projects = [
-    {
-      num: 1,
-      title: "Portfolio",
-      desc: "My portfolio",
-      link: "https://tugdual.kerdrel.com",
-    },
-    {
-      num: 2,
-      title: "Portfolio",
-      desc: "My portfolio",
-    },
-  ]; */
-
   return (
     <div>
       <h1 style="text-align: center; padding: 10px;">
-        Here will be all my projects
+        Here will be able to contact me !
       </h1>
     </div>
   );
