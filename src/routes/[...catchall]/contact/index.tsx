6d1bcb397d2af8425b6bace93a5875095ce16aba
Tugdual de K <tugdualk@hotmail.com>
2023-03-10 13:42:50 +0100

    bouton sendmail

--- src/routes/[...catchall]/contact/index.tsx
+++ src/routes/[...catchall]/contact/index.tsx
@@ -1,10 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import Button from "~/components/button/button";
 
 export default component$(() => {
   return (
     <div>
-      <h1>Do not hesitate any longer!</h1>
+      <h1 style="text-align: center;">Do not hesitate any longer!</h1>
+      <Button text="Send me a mail" to="tugdualk@hotmail.com"/>
     </div>
   );
 });
